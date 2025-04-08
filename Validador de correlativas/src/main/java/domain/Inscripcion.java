76b26a1edb1dd10f199a588377eedbabd2d66b6c
Francisco Mosquera Alfaro <fmosqueraalfaro@frba.utn.edu.ar>
2025-04-08 15:22:56 -0300

    Update Inscripcion.java

--- Validador de correlativas/src/main/java/domain/Inscripcion.java
+++ Validador de correlativas/src/main/java/domain/Inscripcion.java
@@ -1,19 +1,28 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Inscripcion {
 
+  @Setter@Getter
   private Alumno alumno;
+  @Setter@Getter
   private List<Materia>materiasAAnotarse;
 
-  public Inscripcion(Alumno alumno, List<Materia> materiasAAnotarse) {
+  public Inscripcion(Alumno alumno) {
     this.alumno = alumno;
-    this.materiasAAnotarse = materiasAAnotarse;
+    this.materiasAAnotarse = new ArrayList<Materia>();
   }
 
   public Boolean aprobada() {
     return materiasAAnotarse.stream()
         .allMatch(materia -> alumno.cumpleConCorrelativas(materia));
+  }
+
+  public void agregarMateriaAAnotarse(Materia materia) {
+    materiasAAnotarse.add(materia);
   }
 }
