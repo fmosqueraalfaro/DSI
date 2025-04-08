8b206b7ea461bed94a19b445eb5fc336b476d9e3
Francisco Mosquera Alfaro <fmosqueraalfaro@frba.utn.edu.ar>
2025-04-08 15:22:41 -0300

    Update Alumno.java

--- Validador de correlativas/src/main/java/domain/Alumno.java
+++ Validador de correlativas/src/main/java/domain/Alumno.java
@@ -1,21 +1,29 @@
 package domain;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
+import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 public class Alumno {
 
+  @Getter
   private String nombre;
   private Integer legajo;
+  @Setter
   private List<Materia> materiasAprobadas;
 
-  public Alumno(String nombre, Integer legajo, List<Materia> materiasAprobadas) {
+  public Alumno(String nombre, Integer legajo) {
     this.nombre = nombre;
     this.legajo = legajo;
-    this.materiasAprobadas = materiasAprobadas;
+    this.materiasAprobadas = new ArrayList<Materia>();
   }
 
   public Boolean cumpleConCorrelativas(Materia materia) {
     return materiasAprobadas.containsAll(materia.getCorrelativas());
+  }
+
+  public void addMateriaAprobada(Materia materia) {
+    materiasAprobadas.add(materia);
   }
 }
